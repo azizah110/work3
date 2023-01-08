8740d954144cff78c36dce2a882bebfaf422fd9a
azizah110 <116996347+azizah110@users.noreply.github.com>
2023-01-08 09:45:26 +0300

    Initial Commit

---
+++ work3/work3App.swift
@@ -0,0 +1,17 @@
+//
+//  work3App.swift
+//  work3
+//
+//  Created by azizah ahmed alshami on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct work3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
